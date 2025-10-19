f6de75e794e55602138beef4512395fde65b2850
Mohamed-chergui <mohamed.bahae.eddine.chergui@ensia.edu.dz>
2025-10-19 14:11:26 +0100

    fronted set up

---
+++ frontend/lib/screens/add_meal_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AddMealScreen extends StatefulWidget {
+  const AddMealScreen({super.key});
+
+  @override
+  State<AddMealScreen> createState() => _AddMealScreenState();
+}
+
+class _AddMealScreenState extends State<AddMealScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: 
+      Column(
+        children: [
+
+        ],
+      )),
+    );
+  }
+}
---
+++ frontend/lib/screens/add_orders_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AddOrdersScreen extends StatefulWidget {
+  const AddOrdersScreen({super.key});
+
+  @override
+  State<AddOrdersScreen> createState() => _AddOrdersScreenState();
+}
+
+class _AddOrdersScreenState extends State<AddOrdersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: Column(
+        children: [
+          
+        ],
+      )),
+    );
+  }
+}
