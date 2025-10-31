a0dee7bf4538958931059a774ffb63320315a8ea
Mohamed-chergui <mohamed.bahae.eddine.chergui@ensia.edu.dz>
2025-10-31 10:25:41 +0100

    android set up fixed

---
+++ frontend_2/lib/screens/start_screen.dart
@@ -0,0 +1,59 @@
+import 'add_meal_screen.dart';
+import 'add_orders_screen.dart';
+import 'home_screen.dart';
+import 'package:flutter/material.dart';
+
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    AddMealScreen(),
+    AddOrdersScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: _screens[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.lunch_dining),
+            label: 'add meal',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.rounded_corner),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
